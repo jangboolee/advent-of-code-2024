58ce4d8d0e3c125870ac1f8c64305adcd17fc219
Jangboo Lee <github@jangboolee.com>
2024-12-18 06:23:05 +0900

    Upload solution for day 4

--- 04/ceres_search.py
+++ 04/ceres_search.py
@@ -94,8 +94,62 @@ def part_one(data: list[str]) -> int:
     return total
 
 
+def part_two(data: list[str]) -> int:
+
+    def check_x_mas(data: list[str], a_row: int, a_col: int):
+
+        right = False
+        left = False
+
+        # If the top left is either a M or S
+        if data[a_row - 1][a_col - 1] in ("M", "S"):
+            # If the top left is a M
+            if data[a_row - 1][a_col - 1] == "M":
+                # Check that the bottom right is a S
+                if data[a_row + 1][a_col + 1] == "S":
+                    right = True
+                else:
+                    return False
+            # If the top left is a S
+            else:
+                # Check that the bottom right is a M
+                if data[a_row + 1][a_col + 1] == "M":
+                    right = True
+                else:
+                    return False
+        # If the top right is either a M or S
+        if data[a_row - 1][a_col + 1] in ("M", "S"):
+            # If the top right is a M
+            if data[a_row - 1][a_col + 1] == "M":
+                # Check that the bottom left is a S
+                if data[a_row + 1][a_col - 1] == "S":
+                    left = True
+                else:
+                    return False
+            # If the top left is a S
+            else:
+                # Check that the bottom right is a M
+                if data[a_row + 1][a_col - 1] == "M":
+                    left = True
+                else:
+                    return False
+
+        return all([right, left])
+
+    count = 0
+    for row in range(1, len(data) - 1):
+        for col in range(1, len(data[row]) - 1):
+            if data[row][col] == "A":
+                count += check_x_mas(data, row, col)
+
+    return count
+
+
 if __name__ == "__main__":
 
     data = read_input("04")
     part_one_ans = part_one(data)
     print(part_one_ans)
+
+    part_two_ans = part_two(data)
+    print(part_two_ans)
